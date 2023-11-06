678b33b8e3bffd465a71e457dc1f73ab7c3f15f8
a97455 <a97455@alunos.uminho.pt>
2023-11-06 10:37:19 +0000

    self node

--- Node.py
+++ Node.py
@@ -9,6 +9,7 @@ class Node:
     def __init__(self,folder_path,host,port):
         # Dicionario com os nomes dos ficheiros (chave) e uma lista dos blocos que tem desse ficheiro (valor)
         self.dict_files = {}
+        
         for file in os.listdir(folder_path):
             if os.path.isfile(os.path.join(folder_path, file)):
                 self.dict_files[file] = None
@@ -18,35 +19,35 @@ class Node:
         name_count += 1
         
         # Cria o socket TCP
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # Conecta ao servidor
-        client_socket.connect((host, port))
+        self.client_socket.connect((host, port))
         
-    def startConnection(self,client_socket,node_name):
+    def startConnection(self):
         # Mensagens para o servidor
-        tpm.startConnection(client_socket,node_name)
+        tpm.startConnection(self.client_socket,self.node_name)
         # Recebe a resposta do servidor
-        data = client_socket.recv(1024)
+        data = self.client_socket.recv(1024)
         print("Conexão realizada: " + data.decode())
     
-    def filesDictNode(self,client_socket,node_name,dict_files):
+    def filesDictNode(self):
         # Mensagens para o servidor
-        tpm.filesDictNode(client_socket,node_name,dict_files)
-        data = client_socket.recv(1024)
+        tpm.filesDictNode(self.client_socket,self.node_name,self.dict_files)
+        data = self.client_socket.recv(1024)
         print("Ficheiros locais recebidos: " + data.decode())
 
 
-    def getFile(self,client_socket,filename):
+    def getFile(self,filename):
         # Mensagens para o servidor
-        tpm.getFile(client_socket,filename)
+        tpm.getFile(self.client_socket,filename)
         
         while True:
             try:
-                header = client_socket.recv(20).decode().split("|")
+                header = self.client_socket.recv(20).decode().split("|")
 
                 data_length = int(header[1], 16)
-                data = client_socket.recv(data_length)
+                data = self.client_socket.recv(data_length)
                 
                 # message vai ser um dicionário
                 message = json.loads(data.decode())
@@ -55,18 +56,18 @@ class Node:
                     dict_nodeAddress_listBlocks = message['dict_nodeAddress_listBlocks']
 
                     # funcao para transferir o ficheiro ou parte dele de um no(transfer_protocol) 
-                    self.filesDictNode(client_socket,self.node_name,self.dict_files)
+                    self.filesDictNode(self.client_socket,self.node_name,self.dict_files)
                     break
 
             except Exception:
                 continue
 
-    def endConnection(client_socket,node_name):
+    def endConnection(self):
         # Mensagens para o servidor
-        tpm.endConnection(client_socket,node_name)
+        tpm.endConnection(self.client_socket,self.node_name)
 
-        data = client_socket.recv(1024)
+        data = self.client_socket.recv(1024)
         print("Conexão Terminada: " + data.decode())
 
         # Fecha a conexão com o servidor
-        client_socket.close()
+        self.client_socket.close()
